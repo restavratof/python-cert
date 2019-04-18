2d80d473cd6a0294c7805a799c33b36095f99030
dfashchanka <d.faschenko@gmail.com>
2019-04-18 14:28:56 +0300

    adding first steps

---
+++ 1.1_first_program.py
@@ -0,0 +1,18 @@
+print('STEP 1: Print default:')
+print("My name is","Python")
+print("Monthy Python")
+
+print('STEP 2: Print end   (by default end=\\n )')
+print('-'*5)
+print("My name is","Python", end=" ")
+print("Monthy Python")
+print('-'*5)
+print("My name is ", end="")
+print("Monthy Python")
+
+print('STEP 3: Print sep   (by default sep=" " )')
+print("My", "name", "is","Python", sep="-")
+print("My", "name", "is","Python", sep="-", end="*")
+
+
+
---
+++ 1.2_literals.py
@@ -0,0 +1,24 @@
+print('-'*5,'Python allows underscores "_" in numbers')
+print(11_111_11)
+
+print('-'*5,'Octal example')
+print(0o77)
+
+print('-'*5,'Hexadecimal example')
+print(0x77)
+
+print('-'*5,'Floats')
+print(0.4)
+print(.4)
+print(4.0)
+print(4.)
+
+print('-'*5,'Scientific notation')
+# 3x10^8 (It reads: three times ten to the power of eight)
+print(3E8)
+print(3e8)
+# 6.62607 × 10−34
+print(6.62607E-34)
+# Python always chooses the more economical form of the number’s presentation,
+print(0.0000000000000000000001)
+print('-'*5)
