bee69a7df08b04f2479a8934f9de9fcb7da18e73
dfashch <d.faschenko@gmail.com>
2019-05-28 00:45:13 +0300

    4.1 modules initial

--- 4.1_modules.py
+++ 4.1_modules.py
@@ -13,3 +13,35 @@ print(M.sin(M.pi/2))
 print('-'*10,'aliasing 2:')
 from math import sin as sine, pi as P
 print(sine(P/2))
+
+#-----------------------------------------------
+# Some useful modules
+#-----------------------------------------------
+print('-'*50)
+print(' 4.2')
+print('-'*50)
+import os
+print(dir(os)) # list os module functions in a sorted list
+print('-'*10, 'math:')
+print(M.e)
+# ---
+from math import ceil, floor, trunc, factorial, hypot
+x=1.4
+y=2.6
+# ceil(x) - return smallest integer greater or equal to x
+print(ceil(x), ceil(y))
+print(ceil(-x), ceil(-y))
+# floor(x) - return largest integere less than or equal x
+print(floor(x), floor(y))
+print(floor(-x), floor(-y))
+# trunc(x) - the value of x is truncated to an integer
+print(trunc(x), trunc(y))
+print(trunc(-x), trunc(-y))
+# factorial - returns x! (x has to be an integral and not a negative)
+print(factorial(2), factorial(3))
+# hypot(x,y)→ returns the length of the hypotenuse of a right-angle triangle
+# with the leg lengths equal to x and y (the same as sqrt(pow(x,2)+pow(y,2)) but more precise)
+print(hypot(x, y))
+print(hypot(-x, -y))
+
+print('-'*10, 'random:')
