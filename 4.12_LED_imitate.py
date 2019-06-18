3d17c322a5baa127411a798339aae1fac2251071
dfashchanka <d.faschenko@gmail.com>
2019-06-18 16:24:06 +0300

    4.12 add solution with arrays

--- 4.12_LED_imitate.py
+++ 4.12_LED_imitate.py
@@ -1,3 +1,33 @@
+import time
+start1 = time.time()
+
+numbers = [
+    ['###', '#', '###', '###', '# #', '###', '###', '###', '###', '###'],
+    ['# #', '#', '  #', '  #', '# #', '#  ', '#  ', '  #', '# #', '# #'],
+    ['# #', '#', '###', '###', '###', '###', '###', '  #', '###', '###'],
+    ['# #', '#', '#  ', '  #', '  #', '  #', '# #', '  #', '# #', '  #'],
+    ['###', '#', '###', '###', '  #', '###', '###', '  #', '###', '###']
+]
+separator = ' '
+result = [];
+str1 = '1234567890123456789012345678901234567890'
+# str1 = input()
+
+for i in range(5):
+    result_line = separator
+    for ch in (str1):
+        result_line += numbers[i][int(ch)] + separator
+    result.append(result_line)
+# print result
+for i in range(5):
+    print(result[i])
+
+end1 = time.time()
+
+print('')
+
+start2 = time.time()
+
 numbers = {
     0: {'1': '#', '2': '###', '3': '###', '4': '# #', '5': '###', '6': '###', '7': '###', '8': '###', '9': '###', '0': '###'},
     1: {'1': '#', '2': '  #', '3': '  #', '4': '# #', '5': '#  ', '6': '#  ', '7': '  #', '8': '# #', '9': '# #', '0': '# #'},
@@ -5,28 +35,21 @@ numbers = {
     3: {'1': '#', '2': '#  ', '3': '  #', '4': '  #', '5': '  #', '6': '# #', '7': '  #', '8': '# #', '9': '  #', '0': '# #'},
     4: {'1': '#', '2': '###', '3': '###', '4': '  #', '5': '###', '6': '###', '7': '  #', '8': '###', '9': '###', '0': '###'}
 }
-
 separator = ' '
-
 result = [];
-
-
-str1 = '12'
-
-# for ch in (str1):
-#     print(line1[ch][])
-
-str1 = input()
-
+str1 = '1234567890123456789012345678901234567890'
+# str1 = input()
 
 for i in range(5):
     result_line = separator
     for ch in (str1):
         result_line += numbers[i][ch] + separator
     result.append(result_line)
-
-# exit(1)
-
 # print result
 for i in range(5):
     print(result[i])
+
+end2 = time.time()
+
+print(f"ARR : {end1 - start1}")
+print(f"DICT: {end2 - start2}")
