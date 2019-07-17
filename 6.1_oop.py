9bf142337259dbb31a6e21f8dd6d679fe39809f1
dfashch <d.faschenko@gmail.com>
2019-07-17 22:19:41 +0300

    6.1 completed

--- 6.1_oop.py
+++ 6.1_oop.py
@@ -1,14 +1,26 @@
 #------------------------------------------------------------------
+# METHODS:
+#------------------------------------------------------------------
+# __init__      - constructor
+#       Cannot return a value!
+#       Cannot be invoked directly either from the object or from inside the class!
+#
+# type()        - a function to identify
+
+
+#------------------------------------------------------------------
 # VARIABLES:
 #------------------------------------------------------------------
-# __init__  - constructor
-
-
-#------------------------------------------------------------------
-# METHODS
-#------------------------------------------------------------------
-# __dict__  - contains the names and values of all the properties (variables) the object is currently carrying
-
+# __dict__      - contains the names and values of all the properties (variables) the object is currently carrying
+#__name__       - contains name of the class (exists only inside classes)
+#                   To find class of object use method 'type()'
+# __module__    - is a string, too - it stores the name of the module which contains the definition of the class.
+#                   any module named __main__ is actually not a module, but the file currently being run.
+# __bases__     - is a tuple. The tuple contains classes (not class names) which are direct superclasses for the class.
+#                   The order is the same as that used inside the class definition.
+#                   only classes have this attribute - objects don't.
+#
+#
 
 class ExampleClass:
     def __init__(self, val = 1):
@@ -30,3 +42,39 @@ exampleObject3.third = 5
 print(exampleObject1.__dict__)
 print(exampleObject2.__dict__)
 print(exampleObject3.__dict__)
+
+# hasattr - check if object/class contains specified property
+#           expects 2 arguments:
+#               - class/object being checked
+#               - name of property (String) whose existence has to be reported
+#           Returns: True/False
+if hasattr(exampleObject3, 'third'):
+    print(exampleObject3.third)
+
+
+# SELF - is used to:
+#   - obtain access to the object's instance and class variables.
+#   - invoke other object/class methods from inside the class.
+
+print('-'*10)
+# Everything we've said about property name mangling applies to method names too.
+# A method whose name starts with __ is (partially) hidden.
+class Classy:
+    def visible(self):
+        print("visible")
+
+    def __hidden(self):
+        print("hidden")
+
+
+obj = Classy()
+obj.visible()
+
+try:
+    obj.__hidden()
+except:
+    print("failed")
+
+obj._Classy__hidden()
+
+
---
+++ 6.1_reflection_and_introspection.py
@@ -0,0 +1,41 @@
+# Reflection and introspection
+#     introspection - which is the ability of a program to examine the type or properties of an object at runtime;
+#     reflection    - which goes a step further, and is the ability of a program to manipulate the values,
+#                       properties and/or functions of an object at runtime.
+
+class MyClass:
+    pass
+
+obj = MyClass()
+obj.a = 1
+obj.b = 2
+obj.i = 3
+obj.ireal = 3.5
+obj.integer = 4
+obj.z = 5
+
+def incIntsI(obj):
+    for name in obj.__dict__.keys():
+        if name.startswith('i'):
+            val = getattr(obj, name)
+            if isinstance(val, int):
+                setattr(obj, name, val + 1)
+
+print(obj.__dict__)
+incIntsI(obj)
+print(obj.__dict__)
+
+# This is how it works:
+#     line 1: define a very simple class...
+#     lines 3 through 10: ... and fill it with some attributes;
+#     line 12: this is our function!
+#     line 13: scan the __dict__ attribute, looking for all attribute names;
+#     line 14: if a name starts with i...
+#     line 15: ... use the getattr() function to get its current value; note: getattr() takes two arguments:
+#               an object, and its property name (as a string), and returns the current attribute's value;
+#     line 16: check if the value is of type integer, and use the function isinstance() for this purpose (we'll discuss this later);
+#     line 17: if the check goes well, increment the property's value by making use of the setattr() function;
+#               the function takes three arguments: an object, the property name (as a string), and the property's new value.
+
+
+
