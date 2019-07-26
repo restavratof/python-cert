2e3e0655b08d10713c4df56e051dcf5049ff6057
dfashchanka <d.faschenko@gmail.com>
2019-07-26 17:52:27 +0300

    fibonacci

--- 6.2_generators.py
+++ 6.2_generators.py
@@ -1,1 +1,18 @@
 # range() - function is, in fact, a generator, which is (in fact, again) an iterator
+
+# Genrator Fibonacci
+inputNumber = 10
+
+print('-'*10)
+def fibonacci(max):
+    a, b = 0, 1
+    for _ in range(max):
+        yield a
+        a, b = b, a + b
+
+print('In column:')
+for i in fibonacci(inputNumber):
+    print(i)
+
+print('In list:')
+print(list(fibonacci(inputNumber)))
