90c0f052c934dd1b3f5d09f682881e144f6c4c8f
dfashch <d.faschenko@gmail.com>
2019-08-28 01:07:13 +0300

    7.3 done

---
+++ 7.3_chars_freq_histogram.py
@@ -0,0 +1,25 @@
+from os import strerror
+
+# srcname = input("Source file name?: ")
+srcname = '7.1_test_file.txt'
+
+chars_freq = {}
+
+try:
+    src = open(srcname, 'rt')
+    ch = src.read(1).lower()
+    while ch != '':
+        # print(ch, end='')
+        if ch in chars_freq:
+            chars_freq[ch] = chars_freq[ch] + 1
+        else:
+            chars_freq[ch] = 1
+        ch = src.read(1).lower()
+    src.close()
+except IOError as e:
+    print("Cannot open source file: ", strerror(e.errno))
+    exit(e.errno)
+
+for key, val in chars_freq.items():
+    print('"',key,'" -> ', val)
+
